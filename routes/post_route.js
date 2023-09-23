749a939b831eb2bf21f1f861a4626beb995d195f
madhav <madhav201singh@gmail.com>
2023-09-23 20:56:00 +0530

    create-post

--- routes/post_route.js
+++ routes/post_route.js
@@ -1,6 +1,6 @@
-// Create the api for getting all the post..
-// Let's create the api for getting the posts by userss.
-// Here we will refer to the PostModel....
+// Create an API endpoint to retrieve all posts.
+// Now, let's also create an API to fetch posts by specific users, utilizing the PostModel.
+
 app.get('/allposts',async(req,res)=>{
     try{
         const dbPost = await PostModel.find()
@@ -12,16 +12,41 @@ app.get('/allposts',async(req,res)=>{
         res.status(505).json({error:err.message})
     }
 })
+
 // Create an API endpoint for "myAllPosts" that retrieves posts belonging to the current user.
 // For example, when a user accesses their profile, they can view all their own posts.
 
-app.get("/myallposts", protectedRoute, async (req, res) => {
+app.get('/myallposts',protectedRoute, async(req,res)=>{
+    try{
+        const dbPosts = await PostModel.find({author:req.user._id})
+        .populate('author','_id fullname profileImg');
+        res.status(200).json({posts:dbPosts})
+    }
+    catch(err){
+        res.status(505).json({error:err.message})
+    }// this is simple api endpoint for getting all user post...
+})
+
+
+// Define API endpoints for creating posts.
+
+router.post("/createpost", protectedRoute, async (req, res) => {
+    const { description, location, image } = req.body;
+    
     try {
-        const dbPosts = await PostModel.find({ author: req.user._id })
-            .populate("author", "_id fullName profileImg");
-        res.status(200).json({ posts: dbPosts });
+        if (!description || !location || !image) {
+            return res.status(400).json({ error: "One or more mandatory fields are empty" });
+        }
+
+        req.user.password = undefined;
+        const postObj = new PostModel({ description, location, image, author: req.user });
+        const newPost = await postObj.save();
+
+        res.status(201).json({ post: newPost });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "An error occurred while fetching user's posts." });
+        res.status(500).json({ error: "An error occurred while creating the post." });
     }
 });
+
+
