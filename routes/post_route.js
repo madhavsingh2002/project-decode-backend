4a8c255672a41a5fb10c5c7ebcdf48eea80df7f8
madhav <madhav201singh@gmail.com>
2023-09-24 10:06:52 +0530

    delete-post

--- routes/post_route.js
+++ routes/post_route.js
@@ -30,22 +30,43 @@ app.get('/myallposts',protectedRoute, async(req,res)=>{
 
 // Define API endpoints for creating posts.
 
-router.post("/createpost", protectedRoute, async (req, res) => {
-    const { description, location, image } = req.body;
-    
+// Create this endpoints.....
+app.post('/createpost',protectedRoute,async(req,res)=>{
+    const {description,location,image}=req.body;
+    try{
+        if(!description || !location || !image){
+            return res.status(404).json({error:'One or more mandatory fields are empty'})
+        }
+        req.user.password =undefined;
+        const postObj = new PostModel({description,location,image,author:req.user});
+        const newPost = await postObj.save()
+        res.status(201).json({post:newPost})
+    }
+    catch(err){
+        res.status(500).json({error:err.message})
+    }
+})
+
+// Define API endpoints for deleting posts.
+
+app.delete("/deletepost/:postId", protectedRoute, async (req, res) => {
     try {
-        if (!description || !location || !image) {
-            return res.status(400).json({ error: "One or more mandatory fields are empty" });
+        const postFound = await PostModel.findOne({ _id: req.params.postId }).populate("author", "_id");
+        
+        if (!postFound) {
+            return res.status(400).json({ error: "Post does not exist" });
         }
 
-        req.user.password = undefined;
-        const postObj = new PostModel({ description, location, image, author: req.user });
-        const newPost = await postObj.save();
-
-        res.status(201).json({ post: newPost });
+        // Check if the post author is the same as the logged-in user; only then allow deletion
+        if (postFound.author._id.toString() === req.user._id.toString()) {
+            await postFound.remove();
+            res.status(200).json({ result: "Post deleted successfully" });
+        } else {
+            res.status(403).json({ error: "You are not authorized to delete this post" });
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "An error occurred while creating the post." });
+        res.status(500).json({ error: "An error occurred while deleting the post." });
     }
 });
 
